2cdb4f29d41378e8a834c7390595a2c6cd9f186d
Dany Haddad <danyhaddad43@gmail.com>
2022-09-21 10:57:18 -0500

    orgmode changes

--- personal/org-config.el
+++ personal/org-config.el
@@ -7,55 +7,8 @@
 
 (setf org-agenda-include-diary t)
 
-(require 'appt)
-(setf appt-time-msg-list nil)
-(setf appt-display-interval '2)
-(setf appt-message-warning-time '10  ;; send first warning 10 minutes before appointment
-      appt-display-format 'window)   ;; pass warnings to the designated window function
-(appt-activate 1)
-
-(org-agenda-to-appt)
-(run-at-time "24:01" 3600 'org-agenda-to-appt)           ;; update appt list hourly
-(add-hook 'org-finalize-agenda-hook 'org-agenda-to-appt) ;; update appt list on agenda view
-
-;; set up the call to terminal-notifier
-(defvar my-notifier-path "notify-send")
-(defun my-appt-send-notification (title msg)
-  (cond
-   ((string= system-type "gnu/linux")
-    (shell-command
-     (concat my-notifier-path " -u normal " msg " " title)))
-   ((string= system-type "darwin")
-    (progn
-      (message (concat "osascript -e 'display notification \""
-                       (substring msg 1 -1)
-                       "\" with title \"Org mode "
-                       (substring title 1 -1) "\"'" ))
-      (shell-command
-       (concat "osascript -e 'display notification \""
-               (substring msg 1 -1)
-               "\" with title \"Org mode "
-               (substring title 1 -1) "\"'" ))))))
-
-(defun my-appt-display (min-to-app new-time msg)
-  (if (listp msg)
-      (cl-mapc (lambda (min-to-app new-time msg)
-                 (my-appt-send-notification
-                  (format "'Appointment in %s minutes'" (substring-no-properties min-to-app))
-                  (format "'%s'" (substring-no-properties msg))))
-               min-to-app
-               new-time
-               msg)
-    (my-appt-send-notification
-     (format "'Appointment in %s minutes'" min-to-app)
-     (format "'%s'" (substring-no-properties msg)))))
-
-(setq appt-disp-window-function (function my-appt-display))
 (set-face-foreground 'org-hide "DimGray")
 (set-face-background 'org-scheduled-previously "#3F3F3F")
-
-(setf org-clock-persist 'history)
-(org-clock-persistence-insinuate)
 
 (setf org-clock-into-drawer 3)
 
@@ -78,41 +31,7 @@
         ;;  "* %?\nEntered on %U\n  %i\n  %a\nSCHEDULED: %^t")
         ("s" "Appointment" entry
          (file+headline ,(concat my-org-path "gtd.org") "Appointments")
-         "* %?\nSCHEDULED: %(org-insert-time-stamp (org-read-date nil t) t)")
-        ))
-
-(org-babel-do-load-languages 'org-babel-load-languages '((awk . t)
-                                                         (C . t)
-                                                         (R . t)
-                                                         (calc . t)
-                                                         (clojure . t)
-                                                         (css . t)
-                                                         (dot . t)
-                                                         (emacs-lisp . t)
-                                                         (fortran . t)
-                                                         (gnuplot . t)
-                                                         (haskell . t)
-                                                         (java . t)
-                                                         (js . t)
-                                                         (latex . t)
-                                                         (ledger . t)
-                                                         (lisp . t)
-                                                         (makefile . t)
-                                                         (maxima . t)
-                                                         (matlab . t)
-                                                         (mscgen . t)
-                                                         (ocaml . t)
-                                                         (octave . t)
-                                                         (org . t)
-                                                         (perl . t)
-                                                         (picolisp . t)
-                                                         (python . t)
-                                                         (ruby . t)
-                                                         (sass . t)
-                                                         (scheme . t)
-                                                         (screen . t)
-                                                         (sql . t)
-                                                         (sqlite . t)))
+         "* %?\nSCHEDULED: %(org-insert-time-stamp (org-read-date nil t) t)")))
 
 (setq org-agenda-files (list my-org-path))
 (setq org-refile-targets '((org-agenda-files . (:maxlevel . 2))))
